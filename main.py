9231200028dbc593cd297eba5c088ae3fac8fa18
Jayce <captn.dev@protonmail.com>
2024-12-13 23:16:47 +0000

    voice detection for "jarvis clip that"

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ import playsound
 import speech_recognition as sr
 from gtts import gTTS
 
-
 # call speak and input what the TTS should say
 # save output as "test.mp3"
 def speak(text):
     tts = gTTS(text = text, lang = "en")
-    filename = "test.mp3"
+    filename = "audio.mp3"
     tts.save(filename)
-    playsound.playsound(filename)
-    
-speak("Test")
+    playsound.playsound(filename)    
+
+def get_audio():
+    recog = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = recog.listen(source)
+        said = ""
+        
+        try:
+            said = recog.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("exception: " + str(e))
+
+    return said
+
+text = get_audio()
+if "Jarvis clip that" in text:
+    speak("Recieved")
